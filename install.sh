5e53c5efb680c535f8df91fe8d4eb9666f0406c0
Matthieu CHARETTE <matthieu.charette@gmail.com>
2020-04-04 19:10:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ clear
 cd
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo pip3.7 install rpi_ws281x adafruit-circuitpython-neopixel
+sudo pip install rpi_ws281x adafruit-circuitpython-neopixel
 sudo bash -c 'echo "blacklist snd_bcm2835" >> /etc/modprobe.d/snd-blacklist.conf'
 sudo reboot
